21337470d2d40ca8f332745458a1b34e7843385d
caissel <adaslesiak@gmail.com>
2016-08-04 17:04:42 +0200

    Move to Maven project, Apache HTTP Components

--- VismaOAuthConnector/src/com/adamlesiak/visma/ClientDataFetcher.java
+++ VismaOAuthConnector/src/com/adamlesiak/visma/ClientDataFetcher.java
@@ -3,11 +3,20 @@ package com.adamlesiak.visma;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
 /**
  * 
@@ -50,40 +59,26 @@ public class ClientDataFetcher {
 	 */	
 	public void requestAccessToken(String tokenEndpoint) throws IOException {
 						
-		URL url = new URL(tokenEndpoint);		
-		Map<String,String> params = new HashMap<String, String>();
-		StringBuffer postData = new StringBuffer();
-		StringBuffer accessTokenBuffer = new StringBuffer();
+		StringBuffer accessTokenBuffer = new StringBuffer();			
+		HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(tokenEndpoint);
 		
-		params.put("client_id", client.getClientId());
-		params.put("client_secret", client.getClientSecret());
-		params.put("code", code);
-		params.put("grant_type", client.getGrantTypeAuhorizationCode());
-		params.put("redirect_uri", client.getRedirectURI());
-		
-		postData.append('&');
-		for (Map.Entry<String, String> param : params.entrySet()) {
-		    postData.append(param.getKey());
-		    postData.append('=');
-		    postData.append(param.getValue());
-		}
-		byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-		
-		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-		connection.setRequestMethod("POST");
-		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-		connection.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-		connection.setDoOutput(true);
-		connection.getOutputStream().write(postDataBytes);
-		
-		
-		Reader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-		for (int row; (row = inputReader.read()) >= 0;) {
-			accessTokenBuffer.append((char) row);
-		}
-		
-		accessToken = accessTokenBuffer.toString();
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+        nameValuePairs.add(new BasicNameValuePair("grant_type", client.getGrantTypeAuhorizationCode()));
+        nameValuePairs.add(new BasicNameValuePair("code", code));
+        nameValuePairs.add(new BasicNameValuePair("client_id", client.getClientId()));
+        nameValuePairs.add(new BasicNameValuePair("client_secret", client.getClientSecret()));
+        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        
+        HttpResponse response = httpClient.execute(post);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+        	accessTokenBuffer.append(line);
+        }
 
+		accessToken = accessTokenBuffer.toString();
 	}
 		
 	public Client getClient() {
