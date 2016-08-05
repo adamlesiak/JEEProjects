1cb51d8cc2f7829968a0383882127a3f3df40dfa
caissel <adaslesiak@gmail.com>
2016-08-05 16:53:26 +0200

    Saving data by POST

--- VismaOAuthConnector/src/com/adamlesiak/visma/ClientDataFetcher.java
+++ VismaOAuthConnector/src/com/adamlesiak/visma/ClientDataFetcher.java
@@ -3,21 +3,25 @@ package com.adamlesiak.visma;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
-
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -65,7 +69,6 @@ public class ClientDataFetcher {
 	 * @throws IOException 
 	 * @throws AuthenticationException 
 	 */	
-	@SuppressWarnings("deprecation")
 	public void requestAccessToken(String tokenEndpoint) throws IOException, AuthenticationException {
 						
 		StringBuffer accessTokenBuffer = new StringBuffer();			
@@ -94,8 +97,37 @@ public class ClientDataFetcher {
 		}
 		
 		JSONObject jsonObject = new JSONObject(accessTokenBuffer.toString());
-		accessToken = jsonObject.getString(accesTokenParameterName);
+		accessToken = jsonObject.getString(accesTokenParameterName);		
+	}
+	
+	public void sendDataJSON(String url, String accesToken, String JSON) throws IOException {
+	
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(url);
+	    		
+		StringEntity entity = new StringEntity(JSON, ContentType.APPLICATION_JSON);
+		post.setEntity(entity);
+		post.setHeader("Authorization", "Bearer " + accesToken);
+		///post.setHeader("Content-type", "application/json");
+				
+		HttpResponse response = httpClient.execute(post);
+		System.out.println("SEND POST RESPONSE: " + response);
+		System.out.println("URI: " + post.getURI());
+		System.out.println("Request line: " + post.getRequestLine());
 		
+		Header[] headers = response.getAllHeaders();
+		for(Header header : headers) {
+			System.out.println(header.toString());
+		}
+		System.out.println("Acces token: " + accesToken);
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		
+		String line = "";
+		System.out.println("SERVER RESPONSE: ");
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
 	}
 		
 	public Client getClient() {
